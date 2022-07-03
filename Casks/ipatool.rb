0e4bd4266d630b1c74a26ba0c88bf499fdcdf88c
Majd <majd@alfhaily.me>
2022-07-03 14:48:35 +0000

    Update ipatool to v1.1.4

--- Casks/ipatool.rb
+++ Casks/ipatool.rb
@@ -1,8 +1,8 @@
 cask "ipatool" do
-  version "1.1.3"
-  sha256 "b8bc53b7584dfb50a1689bebcb9d691a8b23f0f948b53c89533bde1623dab5af"
+  version "1.1.4"
+  sha256 "aeda77856305bfea52ad1fb0dc288d9c697ec37d9a400391020df80dc7beef10"
 
-  url "https://github.com/majd/ipatool/releases/download/v1.1.3/ipatool-macos-v1.1.3.zip"
+  url "https://github.com/majd/ipatool/releases/download/v1.1.4/ipatool-macos-v1.1.4.zip"
   name "IPATool"
   desc "CLI tool for searching and downloading iOS app packages from the App Store"
   homepage "https://github.com/majd/ipatool"
