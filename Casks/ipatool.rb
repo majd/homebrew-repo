933088489a6a98414a16d653db69ab5d55258fca
Majd Alfhaily <majd@alfhaily.me>
2022-12-08 18:50:04 +0100

    Update ipatool cask format

--- Casks/ipatool.rb
+++ Casks/ipatool.rb
@@ -1,15 +1,20 @@
 cask "ipatool" do
-  version "1.1.4"
-  sha256 "aeda77856305bfea52ad1fb0dc288d9c697ec37d9a400391020df80dc7beef10"
+  if Hardware::CPU.arm?
+    sha256 "aeda77856305bfea52ad1fb0dc288d9c697ec37d9a400391020df80dc7beef10"
+    url "https://github.com/majd/ipatool/releases/download/v1.1.4/ipatool-macos-v1.1.4.zip"
+    binary "ipatool", target: "ipatool"
+  else
+    sha256 "aeda77856305bfea52ad1fb0dc288d9c697ec37d9a400391020df80dc7beef10"
+    url "https://github.com/majd/ipatool/releases/download/v1.1.4/ipatool-macos-v1.1.4.zip"
+    binary "ipatool", target: "ipatool"
+  end
 
-  url "https://github.com/majd/ipatool/releases/download/v1.1.4/ipatool-macos-v1.1.4.zip"
+  version "1.1.4"
   name "IPATool"
   desc "CLI tool for searching and downloading iOS app packages from the App Store"
   homepage "https://github.com/majd/ipatool"
 
   depends_on macos: ">= :catalina"
-
-  binary "ipatool"
 
   postflight do
     system "xattr", "-d", "com.apple.quarantine", "#{HOMEBREW_PREFIX}/bin/ipatool"
